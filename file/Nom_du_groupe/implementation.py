de02f387fee3a919ec0e6349075eb8f6cd0d9161
Rémi <fortierremi08@gmail.com>
2026-04-01 09:09:25 -0400

    l'implementtion est completer

--- file/Nom_du_groupe/implementation.py
+++ file/Nom_du_groupe/implementation.py
@@ -2,5 +2,25 @@ class File:
     def __init__(self, base_de_donnee):
         self.base_de_donnee = base_de_donnee
     
-    def ajouter_queue(self, valeur):
-        
+    def est_vide(self):
+        if len(self.base_de_donnee) == 0:
+            return True
+        return False
+    
+    def enqueue(self, valeur):
+        self.base_de_donnee.append(valeur)
+
+    def dequeue(self):
+        if self.est_vide():
+            return "Erreur: La file est vide"
+        else:
+            self.base_de_donnee.pop(0)
+    
+    def size(self):
+        return len(self.base_de_donnee)
+
+    def front(self):
+        if self.est_vide():
+            return "Erreur: La file est vide"
+        else:
+            return self.base_de_donnee[0]
