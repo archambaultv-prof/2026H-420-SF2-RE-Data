418858a5871958dca92d46f3794ed4a70150ae70
SuperMamimou <fortierremi08@gmail.com>
2026-04-22 08:32:41 -0400

    debut du travail retirer Herobrine

---
+++ liste_chainee/Nom_du_groupe/implementation.py
@@ -0,0 +1,29 @@
+class 
+
+
+
+class Chained_List:
+    def __init__(self, values, pointer):
+        self.values = values
+        self.pointer = pointer
+    def first_add():
+        pass
+    def last_add():
+        pass
+    def iter():
+        pass
+    def un_iter():
+        pass
+    def is_first():
+        pass
+    def is_last():
+        pass
+    def pointer_first():
+        pass
+    def pointer_last():
+        pass
+    def lenght():
+        pass
+    def remove():
+        pass
+    def
